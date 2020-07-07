dd50a9760f599c01ad0c5a186c996458818fb2f0
durashere <walendes@gmail.com>
2020-07-07 14:20:53 +0200

    course-info done

---
+++ part1/course-info/src/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((ele, idx) => (
+        <Part name={ele.name} exercises={ele.exercises} key={idx} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, cur) => acc + cur.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
