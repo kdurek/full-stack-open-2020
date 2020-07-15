260479b948d8ca178e24528707e9a9f6f32c334f
durashere <walendes@gmail.com>
2020-07-15 15:51:11 +0200

    changed name from course-info to courseinfo

--- part3/phonebook-frontend/src/services/persons.js
+++ part3/phonebook-frontend/src/services/persons.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios'
 const baseUrl =
-  process.env.REACT_APP_BASE_URL || "http://localhost:3001/api/persons";
+  process.env.REACT_APP_BASE_URL || 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-  return axios.get(baseUrl);
-};
+  return axios.get(baseUrl)
+}
 
 const create = (personObject) => {
-  return axios.post(baseUrl, personObject);
-};
+  return axios.post(baseUrl, personObject)
+}
 
 const update = (id, personObject) => {
-  return axios.put(`${baseUrl}/${id}`, personObject);
-};
+  return axios.put(`${baseUrl}/${id}`, personObject)
+}
 
 const remove = (id) => {
-  return axios.delete(`${baseUrl}/${id}`);
-};
+  return axios.delete(`${baseUrl}/${id}`)
+}
 
-export default { getAll, create, update, remove };
+export default { getAll, create, update, remove }
